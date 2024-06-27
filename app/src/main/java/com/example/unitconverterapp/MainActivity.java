cc4fcedc4ccdaee4ffb4dfdf5cd4ca5e2d2f1a1d
Sukriti <sonal147sukriti@gmail.com>
2024-06-27 15:53:41 +0530

    Final Commit

---
+++ app/src/main/java/com/example/unitconverterapp/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.unitconverterapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    Button btn;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        btn = findViewById(R.id.btn);
+        textView = findViewById(R.id.textView);
+
+        // Adding the Functionalities
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Getting the user input (in Kilos)
+                String inputText = editText.getText().toString();
+
+                // Converting a string into double
+                double kilos = Double.parseDouble(inputText);
+
+                // Converting Kilos to Pounds
+                double pounds = makeConversion(kilos);
+
+                // Displaying the conversion result
+                textView.setText("" +pounds);
+            }
+        });
+    }
+
+    public double makeConversion(double kilos){
+        // 1 kilo = 2.20462;
+
+        return kilos * 2.20462;
+
+    }
+}
